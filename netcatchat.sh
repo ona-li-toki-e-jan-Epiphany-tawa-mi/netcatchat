d9ec6219ff433dc07bb59f3107c5451b808b4c29
ona-li-toki-e-jan-Epiphany-tawa-mi <darklorddudeal.deaddear@gmail.com>
2023-08-25 03:08:18 -0400

    Added command line args and usage message

--- netcatchat.sh
+++ netcatchat.sh
@@ -1,17 +1,87 @@
 #!/usr/bin/env bash
 
-################################################################################
-# Config START                                                                 #
-################################################################################
+#TODO Make use system user name or something else.
+
+#TODO Add description.
+print_usage() {
+    printf "NAME
+\t%s - simple chat server and client using netcat
+
+SYNOPSIS
+\t%s -h
+\t%s [-p server_port] [-i server_ip]
+\t%s -s [-p server_port] [-c client_ports]
+
+DESCRIPTION
+\tTODO
+
+OPTIONS
+\t-s
+\t\tBy default, %s will run in client mode and try to connect to a
+\t\tserver. Specifying -s will, instead, make it run in server mode.
+
+\t-p server_port
+\t\tIn server mode, %s will listen on server_port for incoming chat
+\t\tclients and routes them to a client port if one is avalible. On client
+\t\tmode, %s will try to connect to the server on server_port to
+\t\tfigure out which client port to connect on. Defaults to 2000
+
+\t-c client_ports
+\t\tServer mode only. client_ports are the avalible ports for clients to
+\t\tconnect on. Each client needs their own port, so the maximum number of
+\t\tusers will be limited by how many are supplied. Defaults to 2001-2010
+\t\t(inclusive.)
+
+\t-i server_ip
+\t\tClient mode only. Will try to connect to the server at server_ip. Defaults
+\t\tto 127.0.0.1, localhost.
+
+\t-h
+\t\tDisplays help text and exits.
+
+AUTHOR
+\tona li toki e jan Epiphany tawa mi.
+
+BUGS
+\tReport bugs to 
+\t<https://github.com/ona-li-toki-e-jan-Epiphany-tawa-mi/netcatchat/issues>.
+
+COPYRIGHT:
+\tCopyright © 2023 ona li toki e jan Epiphany tawa mi. License: MIT. This is
+\tfree software; you are free to modify and redistribute it. See the source
+\tor visit <https://mit-license.org> for the full terms of the license. THIS
+\tSOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND.
+
+SEE ALSO:
+\tGitHub repository:
+\t<https://github.com/ona-li-toki-e-jan-Epiphany-tawa-mi/netcatchat>
+" "$0" "$0" "$0" "$0" "$0" "$0"
+}
+
+# The port that users connect to in order to get a port to chat on.
 server_port=2000
 # shellcheck disable=SC2207
+# (server) The ports that each user uses to send and recieve messages.
 client_ports=($(seq 2001 2010))
-################################################################################
-# Config END                                                                   #
-################################################################################
+# (client) IP of the server to connect to.
+server_ip=127.0.0.1
+# either 'client' or 'server'.
+type=client
 
-#TODO Create proper CLI.
-#TODO Make use system user name.
+# TODO add error checks.
+# TODO add version message.
+while getopts 'sp:c:i:h' flag; do
+    # shellcheck disable=SC2206
+    case "$flag" in
+        s) type=server            ;;
+        p) server_port="$OPTARG"  ;;
+        c) client_ports=($OPTARG) ;;
+        i) server_ip="$OPTARG"    ;;
+        h) print_usage; exit      ;;
+        *) print_usage; exit      ;;
+    esac
+done
+
 
 
 run_server() {
@@ -112,18 +182,18 @@ run_server() {
 
 
 run_client() {
-    #TODO add way to choose ip.
-    echo "Connecting to 127.0.0.1:$server_port..."
-    port=$(netcat -w 0 127.0.0.1 "$server_port" -v)
-    echo "Recieved port $port, reconnecting to 127.0.0.1:$port..."
-    netcat 127.0.0.1 "$port" -v
+    #TODO add error checks
+    echo "Connecting to $server_ip:$server_port..."
+    port=$(netcat -w 0 "$server_ip" "$server_port" -v)
+    echo "Recieved port $port, reconnecting to $server_ip:$port..."
+    netcat "$server_ip" "$port" -v
 }
 
 
 
-# TODO command line stuff with getopts to handle selecting type.
-if [ "$1" = "server" ]; then
+# TODO add error thingy here.
+if [ "$type" = "server" ]; then
     run_server
-elif [ "$1" = "client" ]; then
+elif [ "$type" = "client" ]; then
     run_client
 fi
